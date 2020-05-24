7644c5481b2459bae8cef253c9ea1de9c65ac219
Ovgodd <c.gromoff@gmail.com>
2020-05-24 17:08:44 +0200

    convert_monnaie.py

---
+++ convert_monnaie.py
@@ -0,0 +1,10 @@
+req_one = int(input("Hello what is the amount that you want to covert : "))
+req_two = str(input("what is your currency ? "))
+req_three = str(input("what currency do you want ? exemple \"dollars\" : "))
+req_four = float(input("what is the price( cours )of the currency that you want ? you can check it on google exemple : \"1 dollars to pound price\" : "))
+
+def monney(num1=int(req_one),num2=float(req_four)):
+    return round(num1 * num2)
+
+print ("You want to convert " + str(req_one) + " " + str(req_two) + ".")
+print ("you will have : " + str(monney()) + " " + str(req_three) + ".")
