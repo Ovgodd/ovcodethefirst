8715d564b948a4be68e844be84daaa98990f8359
Ovgodd <c.gromoff@gmail.com>
2020-06-07 19:27:22 +0200

    Car_rental.py

--- Car_rental.py
+++ Car_rental.py
@@ -1,68 +1,37 @@
-#class disponible_car
-#Layer of absaction : disponible car, car price, too add a car, to lend a car
-#class customer
-#choose a car, return a car
+#class car,
+#layer of abstraction : disponible car / fare details / fare calculs
+#object Car
 
-class car:
+class Car:
+    def __init__(self):
+        self.carFare = {'Hatchback' : 30, 'Sedan':50, 'SUV': 100}  #car prices listed with names
 
-    def __init__(self,listOfCars):
-        self.availableCars = listOfCars
+    def displayFareDetails(self):
+        print("Cost per day: ")
+        print("Hatchback : $",self.carFare['Hatchback'])
+        print("Sedan : $",self.carFare['Sedan'])
+        print("SUV : $",self.carFare['SUV'])
 
-        def disponibleCar(self):
-            print()
-            print("Avaiables Cars : ")
-            for cars in self.availableCar:
-                print(cars)
+    def calculateFare(self, typeOfCar, numberOfDays):#calculate the fare depending of the cars and the days that user choice
 
-        def addCar(self, returnedCar):
-            self.availableCar.append(returnedCar) 
-
-        def lendCar(self, chooseCar):
-            if chooseCar in availableCar:
-                print("ok you have rent a car")
-                self.availableCar.remove(chooseCar)
-
-        def prices(self,dayPrice):
-            for price in dayPrice:
-                customerPrice = dayPrice * prices
-        
-        
+        return self.carFare[typeOfCar] * numberOfDays
 
 
-class customer:
-
-    def chooseCar(self):
-        print("please what car do you want ? ")
-        self.cars = input()
-        return self.cars
-        
-
-    def returnedCar(self):
-        print("please what car do you want to return ? ")
-        self.cars = input()
-        return self.cars
-
-    def dayPrice(self):
-        print("how many day do you want your car ? ")
-        self.cars = input()
-        return self.cars
-
-Car = car(['Hatchback','Sedan','SUV'])
-Customer = customer()
-
+car = Car()
 while True:
-    print("Enter 1 to display the car ")
-    print("Enter 2 to request a car")
-    print("Enter 3 to return a car")
-    print("Enter 4 to exit")
-    userChoice = int(input())
+    print("Enter 1 to display the fare details")
+    print("Enter 2 to rent a car")
+    print("Enter 3 to exit")
+    userChoice = (int(input()))
     if userChoice is 1:
-        Car.disponibleCar()
-    elif userChoice is 2:
-        wantedCar = customber.chooseCar
-        customer.lendCar(chooseCar)
+        car.displayFareDetails()
+    if userChoice is 2:
+        print("enter the type of car would you like to want : ")
+        typeOfCar = input()
+        print("please enter the numbers of days would you like to have this car : ")
+        numberOfDays = (int(input()))
+        fare = car.calculateFare(typeOfCar, numberOfDays)
+        print("total payable amount : $",fare)
+        print("Thank you!")
     elif userChoice is 3:
-        returnIt = customer.returnedCar()
-        Car.addCar(returnedCar)
-    elif userChoice is 4:
         quit()
